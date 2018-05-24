d070941a732c636a8f65641748732a7c5c8dedfd
Seba86 <sebadicarlo@github.com>
2018-05-23 21:59:13 -0300

    artist component

--- spotiapp/src/app/services/spotify.service.ts
+++ spotiapp/src/app/services/spotify.service.ts
@@ -4,17 +4,31 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class SpotifyService {
   public artistas:any[]=[];
+  urlSpotify:string='https://api.spotify.com/v1/';
+  tokenSpotify:string='BQDrmn2DJL41xSFQtqzXlzXQccGeWYVzGCkXTLJEN3kTckgwuZDrkmGzt5EJ_OsNgY3yYQThAynVEqRneAI';
+
+  getHeaders():HttpHeaders{
+    let headers= new HttpHeaders({
+          'authorization': 'Bearer '+this.tokenSpotify
+         });
+         return headers;
+  }
 
   constructor(public http: HttpClient) {
       console.log("Servicio listo para usuar.")
    }
+
+   getArtista(id:string){
+     let url=`${this.urlSpotify}artists/${id}`;
+
+     let headers=this.getHeaders();
+      return this.http.get(url,{ headers });
+    }
+
    getArtistas(artistas:string){
-     let url=`https://api.spotify.com/v1/search?query=${artistas}&type=artist&limit=20`;
+     let url=`${this.urlSpotify}search?query=${artistas}&type=artist&limit=20`;
 
-     let headers=new HttpHeaders({
-       'authorization': 'Bearer BQC32q3ZVNUfytWDWzZwnBphTd4NGZnQxW-7_p_qBYpSpWKFbE-cnAQM4wy9lOFp3lNEKPcIo2LznuWpWHc'
-      });
-
+     let headers=this.getHeaders();
       return this.http.get(url,{ headers })
                 .map((resp:any) =>{
                       this.artistas=resp.artists.items;
